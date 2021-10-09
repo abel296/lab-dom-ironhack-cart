096c50a732cab62dab590718dc60f14be6a9b98e
Abel Andrés <abelandres.gtc@gmail.com>
2021-10-09 13:15:52 +0200

    iteration 2(implement update subtotal and calculate all for more than 1 product)

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
-  const priceElement = document.querySelector('.price span')
+  const priceElement = product.querySelector('.price span')
   const price = priceElement.innerHTML
 
-  const quantityElement = document.querySelector('.quantity input')
+  const quantityElement = product.querySelector('.quantity input')
   const quantity = quantityElement.value
 
-  const subtotalElement = document.querySelector('.subtotal span')
+  const subtotalElement = product.querySelector('.subtotal span')
   subtotalElement.innerText = price * quantity
   const subtotal = subtotalElement.innerHTML
 
@@ -20,12 +20,17 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+
+  const products = document.querySelectorAll('.product')
+  products.forEach(elm => {
+    updateSubtotal(elm)
+  })
 
   // ITERATION 3
   //... your code goes here
